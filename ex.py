dea9ae41d32d157d163663b85685db2cd1770099
tiagoangioletti <thangioletti@gmail.com>
2017-06-21 09:32:40 -0300

    Interface util

--- ex.py
+++ ex.py
@@ -1,152 +1,145 @@
 import lex as lex
 
-# List of token names.   This is always required
-reserved = {
-   'VAR' : 'VAR',
-   'PRINT': 'PRINT',
-   'IF' : 'IF',
-   'FOR' : 'FOR',      
-   'WHILE' : 'WHILE',
-   'FUNCTION' : 'FUNCTION',
-   'ELSE' : 'ELSE',   
-   'PROGRAM': 'PROGRAM'      
-}
+class Compiler:
+    def __init__(self, sData=None):
+        # List of token names.   This is always required
+        reserved = {
+           'VAR' : 'VAR',
+           'PRINT': 'PRINT',
+           'IF' : 'IF',
+           'FOR' : 'FOR',      
+           'WHILE' : 'WHILE',
+           'FUNCTION' : 'FUNCTION',
+           'ELSE' : 'ELSE',   
+           'PROGRAM': 'PROGRAM'      
+        }
 
-tokens = [
-    'COMANDO',
-    'STRING',
-    'ID',    
-    'NUM',
-    'DECIMAL',
-    'SOMA',
-    'SUBTRACAO',
-    'MULTIPLICAO',
-    'DIVISAO',
-    'LPAREN',
-    'RPAREN',        
-    'PONTOEVIRGULA',
-    'POTENCIA',
-    'MAIOR',
-    'MENOR',
-    'IGUAL',
-    'MAIORIGUAL',
-    'MENORIGUAL',
-    'DIFERENTE',
-    'ATRIBUICAO',
-    'VIRGULA',
-    'ASPAS'    
+        tokens = [
+            'COMANDO',
+            'STRING',
+            'ID',    
+            'NUM',
+            'DECIMAL',
+            'SOMA',
+            'SUBTRACAO',
+            'MULTIPLICAO',
+            'DIVISAO',
+            'LPAREN',
+            'RPAREN',        
+            'PONTOEVIRGULA',
+            'POTENCIA',
+            'MAIOR',
+            'MENOR',
+            'IGUAL',
+            'MAIORIGUAL',
+            'MENORIGUAL',
+            'DIFERENTE',
+            'ATRIBUICAO',
+            'VIRGULA',
+            'ASPAS'    
 
-] + list(reserved.values())
+        ] + list(reserved.values())
 
-# Regular expression rules for simple tokens
-t_SOMA        = r'\+'
-t_SUBTRACAO   = r'-'
-t_MULTIPLICAO = r'\*'
-t_DIVISAO     = r'/'
-t_LPAREN      = r'\('
-t_RPAREN      = r'\)'
-t_PONTOEVIRGULA = r'\;'
-t_POTENCIA    = r'\^'
-t_MAIOR       = r'\>'
-t_MENOR       = r'\<'
-t_IGUAL       = r'\=='
-t_MAIORIGUAL  = r'\>='
-t_MENORIGUAL  = r'\<='
-t_DIFERENTE   = r'\!='
-t_ATRIBUICAO  = r'\='
-t_VIRGULA     = r'\,'
-t_ASPAS       = r'\"'
+        # Regular expression rules for simple tokens
+        t_SOMA        = r'\+'
+        t_SUBTRACAO   = r'-'
+        t_MULTIPLICAO = r'\*'
+        t_DIVISAO     = r'/'
+        t_LPAREN      = r'\('
+        t_RPAREN      = r'\)'
+        t_PONTOEVIRGULA = r'\;'
+        t_POTENCIA    = r'\^'
+        t_MAIOR       = r'\>'
+        t_MENOR       = r'\<'
+        t_IGUAL       = r'\=='
+        t_MAIORIGUAL  = r'\>='
+        t_MENORIGUAL  = r'\<='
+        t_DIFERENTE   = r'\!='
+        t_ATRIBUICAO  = r'\='
+        t_VIRGULA     = r'\,'
+        t_ASPAS       = r'\"'
 
-def t_COMANDO(t):
-    r'[A-Z_]+'
-    t.type = reserved.get(t.value,'COMANDO')    # Check for reserved words
-    return t
+        def t_COMANDO(t):
+            r'[A-Z_]+'
+            t.type = reserved.get(t.value,'COMANDO')    # Check for reserved words
+            return t
 
-def t_STRING(t):
-    r'"[a-zA-Z_0-9]*"'        
-    return t
+        def t_STRING(t):
+            r'"[a-zA-Z_0-9]*"'        
+            return t
 
-def t_ID(t):
-    r'[i|s|b|f|d|c|o][a-zA-Z_][a-zA-Z_0-9]*'                    
-    return t
+        def t_ID(t):
+            r'[i|s|b|f|d|c|o][a-zA-Z_][a-zA-Z_0-9]*'                    
+            return t
 
-# A regular expression rule with some action code
-def t_NUM(t):
-    r'\d+'
-    t.value = int(t.value)    
-    return t
+        # A regular expression rule with some action code
+        def t_NUM(t):
+            r'\d+'
+            t.value = int(t.value)    
+            return t
 
-# Define a rule so we can track line numbers
-def t_newline(t):
-    r'\n+'
-    t.lexer.lineno += len(t.value)
+        # Define a rule so we can track line numbers
+        def t_newline(t):
+            r'\n+'
+            t.lexer.lineno += len(t.value)
 
-# A string containing ignored characters (spaces and tabs)
-t_ignore  = ' \t'
+        # A string containing ignored characters (spaces and tabs)
+        t_ignore  = ' \t'
 
-# Error handling rule
-def t_error(t):
-    print("Illegal character '%s'" % t.value[0])
-    t.lexer.skip(1)
+        # Error handling rule
+        def t_error(t):
+            print("Illegal character '%s'" % t.value[0])
+            t.lexer.skip(1)
 
-# Build the lexer
-lexer = lex.lex()
+        # Build the lexer
+        lexer = lex.lex()
+            
+        # Give the lexer some input
+        lexer.input(sData)
 
-# Test it out
-sCaminhoImg = 'run.top'
-oArquivo = open(sCaminhoImg)
+        oArquivo = open('tokens.ttop', 'w')
 
-sData = ''
-with oArquivo as oInfo:
-    for sLine in oInfo.readlines():
-        sData = sData+sLine
+        # Tokenize
+        while True:
+            tok = lexer.token()
+            if not tok: 
+                break      # No more input
 
-oArquivo.close()
-# Give the lexer some input
-lexer.input(sData)
+            oArquivo.write(str(tok)+'\n')
 
-oArquivo = open('tokens.ttop', 'w')
+        oArquivo.close()
 
-# Tokenize
-while True:
-    tok = lexer.token()
-    if not tok: 
-        break      # No more input
+        import yacc as yacc
 
-    oArquivo.write(str(tok)+'\n')
+        def p_expreg_soma(p):
+            'expreg : expreg SOMA term'
+            #p[0] = p[1] + p[3]    
 
-oArquivo.close()
+        def p_expreg_subtracao(p):
+            'expreg : expreg SUBTRACAO term'
+            #p[0] = p[1] - p[3]
 
-import yacc as yacc
+        def p_expreg_term(p):
+            'expreg : term'
+            #p[0] = p[1]
 
-def p_expreg_soma(p):
-    'expreg : expreg SOMA term'
-    #p[0] = p[1] + p[3]
+        def p_term_num(p):
+            'term : NUM'
+            #p[0] = p[1]
 
-def p_expreg_subtracao(p):
-    'expreg : expreg SUBTRACAO term'
-    #p[0] = p[1] - p[3]
+        def p_error(p):
+            print("Syntax error in input!")
 
-def p_expreg_term(p):
-    'expreg : term'
-    #p[0] = p[1]
+        parser = yacc.yacc()
 
-def p_term_num(p):
-    'term : NUM'
-    #p[0] = p[1]
+        # Test it out
+        sCaminhoImg = 'run.top'
+        oArquivo = open(sCaminhoImg)
 
-def p_error(p):
-    print("Syntax error in input!")
-
-parser = yacc.yacc()
-
-# Test it out
-sCaminhoImg = 'run.top'
-oArquivo = open(sCaminhoImg)
-
-sData = ''
-with oArquivo as oInfo:
-    for sLine in oInfo.readlines():
-        if not sLine:
-            continue
-        print(parser.parse(sLine))
+        sData = ''
+        with oArquivo as oInfo:
+            for sLine in oInfo.readlines():
+                if not sLine:
+                    continue
+                print(parser.parse(sLine))
+        
--- exec.py
+++ exec.py
@@ -1,8 +1,11 @@
 from tkinter import *
+from util import Util
+from ex import Compiler
 
 class Application:
-	def __init__(self, master=None):
-	
+	util = Util()
+	def __init__(self, master=None):	
+
 		self.fontePadrao = ("Arial", "10")
 		self.primeiroContainer = Frame(master)
 		self.primeiroContainer["height"] = 200
@@ -15,15 +18,16 @@ class Application:
 		self.segundoContainer.pack(side=RIGHT)
 		
 		#TITULO
-		self.titulo = Label(self.primeiroContainer, text="Compilador", justify=LEFT)
-		self.titulo["font"] = ("Arial", "16", "bold")	
-		self.titulo.pack()
+		self.titulo = Label(self.primeiroContainer, text="Compilador", anchor='w')
+		self.titulo["font"] = ('Arial', '16', 'bold')	
+		self.titulo.pack(fill='both')
 
 		#CAIXA DE COMANDOS DA LINGUAGEM		
-		self.codeLabel = Label(self.primeiroContainer,text="Código", font=self.fontePadrao)
-		self.codeLabel.pack()		
+		self.codeLabel = Label(self.primeiroContainer,text="Código:", font=self.fontePadrao, anchor='w')
+		self.codeLabel.pack(fill='both')		
 		
-		self.code = Text(self.primeiroContainer, height=33, width=50)
+		self.code = Text(self.primeiroContainer, height=33, width=70)
+		self.code.insert(END, self.util.getDefaultCode())
 		self.code["font"] = self.fontePadrao
 		self.code.pack()
 
@@ -31,13 +35,13 @@ class Application:
 		self.codeLabel.pack()		
 
 		#BOTÕES
-		self.autenticar = Button(self.primeiroContainer)
-		self.autenticar["text"] = "Compilar"
-		self.autenticar["font"] = ("Calibri", "12")
-		self.autenticar["width"] = 12
-		self.autenticar["pady"] = 5				
-		self.autenticar["command"] = self.verificaSenha
-		self.autenticar.pack(side=LEFT)
+		self.compilar = Button(self.primeiroContainer)
+		self.compilar["text"] = "Compilar"
+		self.compilar["font"] = ("Calibri", "12")
+		self.compilar["width"] = 12
+		self.compilar["pady"] = 5		
+		self.compilar["command"] = self.compila
+		self.compilar.pack(side=LEFT)
 
 		self.space = Label(self.primeiroContainer,text="", font=self.fontePadrao, width=1)
 		self.space.pack(side=LEFT)
@@ -47,26 +51,26 @@ class Application:
 		self.limpar["font"] = ("Calibri", "12")
 		self.limpar["width"] = 12
 		self.limpar["pady"] = 5				
-		self.limpar["command"] = self.verificaSenha
+		self.limpar["command"] = self.limpar
 		self.limpar.pack(side=LEFT)
 
 		#CAIXAS ONDE SERÃO LISTADOS OS DADOS
-		self.lexLabel = Label(self.segundoContainer,text="Lexico", font=self.fontePadrao)
-		self.lexLabel.pack()
+		self.lexLabel = Label(self.segundoContainer,text="Lexico:", font=self.fontePadrao, anchor='w')
+		self.lexLabel.pack(fill='both')
 		
 		self.lex = Text(self.segundoContainer, height=10, width=50)
 		self.lex["font"] = self.fontePadrao
 		self.lex.pack()
 
-		self.sinLabel = Label(self.segundoContainer,text="Sintático", font=self.fontePadrao)
-		self.sinLabel.pack()
+		self.sinLabel = Label(self.segundoContainer,text="Sintático:", font=self.fontePadrao, anchor='w')
+		self.sinLabel.pack(fill='both')
 		
 		self.sin = Text(self.segundoContainer, height=10, width=50)
 		self.sin["font"] = self.fontePadrao
 		self.sin.pack()
 
-		self.semLabel = Label(self.segundoContainer,text="Semantico", font=self.fontePadrao)
-		self.semLabel.pack()
+		self.semLabel = Label(self.segundoContainer,text="Semantico:", font=self.fontePadrao, anchor='w')
+		self.semLabel.pack(fill='both')
 		
 		self.sem = Text(self.segundoContainer, height=10, width=50)
 		self.sem["font"] = self.fontePadrao
@@ -75,14 +79,18 @@ class Application:
 		self.mensagem = Label(self.segundoContainer, text="", font=self.fontePadrao)
 		self.mensagem.pack()
 
-		#Método verificar senha
-	def verificaSenha(self):
-		usuario = self.nome.get()
-		senha = self.senha.get()
-		if usuario == "usuariodevmedia" and senha == "dev":
-			self.mensagem["text"] = "Autenticado"
-		else:
-			self.mensagem["text"] = "Erro na autenticação"
+	def limpar(self):
+		self.code.delete(1.0, END)		    
+		self.lex.delete(1.0, END)
+		self.sin.delete(1.0, END)		    
+		self.sem.delete(1.0, END)
+
+	def compila(self):
+		sData = self.code.get('1.0', END);
+		comp = Compiler(sData)
+		self.lex.delete(1.0, END)
+		self.lex.insert(END, self.util.getTokenFileContent())
+		
 	   
 root = Tk()
 Application(root)
